6d41ff4d16eb5defde8f5dbc10b55a5211935a60
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-01-22 22:15:23 +0700

    Repository

---
+++ repositories/beerRepository.js
@@ -0,0 +1,29 @@
+import client from "../db/database.js";
+
+class BeerRepository {
+  create(beer) {
+    return client.queryArray`INSERT INTO beers (name, brand, is_premium, registration_date) VALUES (${beer.name}, ${beer.brand}, ${beer.is_premium}, ${beer.registration_date})`;
+  }
+
+  selectAll() {
+    return client.queryArray`SELECT * FROM beers ORDER BY id`;
+  }
+
+  selectById(id) {
+    return client.queryArray`SELECT * FROM beers WHERE id = ${id}`;
+  }
+
+  update(id, beer) {
+    const latestBeer = this.selectById(id);
+
+    return client.queryArray`UPDATE beers set name = ${beer.name !== undefined ? beer.name : latestBeer.name}, brand = ${beer.brand !== undefined ? beer.brand : latestBeer.brand}, is_premium = ${
+      beer.is_premium !== undefined ? beer.is_premium : latestBeer.is_premium
+    } WHERE id = ${id}`;
+  }
+
+  delete(id) {
+    return client.queryArray`DELETE FROM beers WHERE id = ${id}`;
+  }
+}
+
+export default new BeerRepository();
